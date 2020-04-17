9567e5dfb1a7a50ba8b85c684cb823c7df10f908
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-04-17 23:19:14 +0200

    Includes hateoas to findById resource.

--- src/main/java/com/lopes/payroll/controller/EmployeeController.java
+++ src/main/java/com/lopes/payroll/controller/EmployeeController.java
@@ -3,9 +3,20 @@ package com.lopes.payroll.controller;
 import com.lopes.payroll.errors.EmployeeNotFoundException;
 import com.lopes.payroll.model.Employee;
 import com.lopes.payroll.model.repository.EmployeeRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /**
  * The type Employee controller.
@@ -56,8 +67,12 @@ public class EmployeeController {
      * @return the employee
      */
     @GetMapping("/{id}")
-    Employee findById(@PathVariable Long id) {
-        return employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    EntityModel<Employee> findById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return new EntityModel<>(employee,
+                linkTo(methodOn(EmployeeController.class).findById(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     /**
